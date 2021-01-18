01cbf8f29386cbbf7e0120a7778bc01e551879ee
Z-Kamili <zakariakamili97@gmail.com>
2021-01-18 20:10:37 +0100

    Ajouter la base de donnes

--- Database/gestion_absence.sql
+++ Database/gestion_absence.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 15, 2021 at 06:38 PM
+-- Generation Time: Jan 18, 2021 at 06:33 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -41,6 +41,39 @@ CREATE TABLE IF NOT EXISTS `presence` (
   KEY `id_apprenant` (`id_apprenant`)
 ) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nom_complet` varchar(20) NOT NULL,
+  `Email` varchar(50) NOT NULL Unique,
+  `Date_debut_scolaire` date DEFAULT NULL,
+  `Date_Fin_scolaire` date DEFAULT NULL,
+  `Role` varchar(40) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `Promo`(
+  `id_promo` int(10) NOT NULL AUTO_INCREMENT,
+  `nom_promo` varchar(20) NOT NULL,
+  id_Dep int,
+  id_user_formateur int,
+  id_user_Apprenant int,
+  PRIMARY KEY (`id_promo`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `Departement`(
+  `id_dep` int(10) NOT NULL AUTO_INCREMENT,
+  `nom_dep` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_dep`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+
 --
 -- Constraints for dumped tables
 --
@@ -51,6 +84,10 @@ CREATE TABLE IF NOT EXISTS `presence` (
 ALTER TABLE `presence`
   ADD CONSTRAINT `presence_ibfk_1` FOREIGN KEY (`id_Formateur`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `presence_ibfk_2` FOREIGN KEY (`id_apprenant`) REFERENCES `user` (`id`);
+ALTER TABLE `Promo`
+  ADD CONSTRAINT `Promo_ibfk_1` FOREIGN KEY (`id_user_formateur`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `Promo_ibfk_2` FOREIGN KEY (`id_user_Apprenant`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `Constraint_id_dep` FOREIGN KEY (`id_Dep`) REFERENCES `Departement` (`id_dep`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
